f318e83782fae56941708796ee6487d816d78b71
Vincent Ly <veely1@gmail.com>
2018-08-05 22:38:44 +0000

    brute force strategy

--- stock-market.js
+++ stock-market.js
@@ -1,3 +1,16 @@
 var maxProfit = function(stock) {
+  var profit = 0;
+  for (let i = 0 ; i < stock.length - 1 ; i++) {
+    if (i + 1 < stock.length && stock[i] < stock[i + 1]) {
+      for (let j = i + 1 ; j < stock.length ; j++ ) {
+        let diff = stock[j] - stock[i];
+        if (diff > profit) {
+          profit = diff;
+        }
+      }
+    }
+  }
+  console.log(profit);
+};
 
-}
+maxProfit([45, 24, 35, 31, 40, 38, 11]);
